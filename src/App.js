8513d12f9954d8b06d3343eaff7c24d648832249
Hugo Noro <noro.hugo@gmail.com>
2017-09-26 14:33:02 +0100

    Created test to check if App is being rendered without crashing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 import {getChatLog, getMembersData} from './service';
-import MessageList from './components/MessageList'
+import MessageList from './components/MessageList';
+import {mergeMessagesWithMembers, sortByTimestamp} from './utils';
 
 import './App.css';
 
@@ -20,37 +21,6 @@ class App extends Component {
         );
     }
 }
-
-export const mergeMessagesWithMembers = (messages, members) => {
-    if(!((messages && messages.length) || (members && members.length)))
-        return {};
-
-    const messageList = messages.map((msg) => {
-        const member = members.find((member) => member.id === msg.userId);
-        return {
-            text: msg.message,
-            email: member.email,
-            msgId: msg.id,
-            avatar: member.avatar,
-            timestamp: msg.timestamp
-        };
-    });
-    return {
-        messageList
-    };
-};
-
-export const sortByTimestamp = (messageList) => {
-    const sortedList = [].concat(messageList)
-        .sort((a,b) => {
-            const dateA = new Date(a.timestamp);
-            const dateB = new Date(b.timestamp);
-            return dateA - dateB;
-        });
-    return {
-        messageList: sortedList
-    }
-};
 
 const mapStateToProps = state => {
     const mergedData = mergeMessagesWithMembers(state.messageState.messages, state.memberState.members);
